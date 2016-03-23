60b8786512d23620626afb93173904631118c36a
Cristobal Dominguez <cristobald@me.com>
2016-03-22 21:38:16 -0300

    Added form

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -9,4 +9,22 @@ class PagesController < ApplicationController
 
   def batman_vs_superman
   end
+
+  def polling
+    if params[:email].present?
+      Vote.create(
+        email: params[:email],
+        heroe: params[:name]
+      )
+
+      redirect_to pages_polling_path, notice: 'Gracias por tu voto!'
+    else
+      flash[:alert] = 'Por favor rellenar el campo de e-mail!'
+      render pages_batman_vs_superman_path
+    end
+  end
+
+  def polling_report
+    @vote = Vote.all
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,17 @@
 Rails.application.routes.draw do
   get 'pages/batman'
-
   get 'pages/superman'
   get 'pages/batman_vs_superman'
+  get 'pages/polling'
+  get 'pages/polling_report'
 
-  root 'pages#batman_vs_superman'
+  
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  root 'pages#batman_vs_superman'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
